3941a4a0d1261f5e02f9d5341067f981cf0fc02f
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-06-07 10:11:01 +0530

    Bubble Sort py

--- JavaScript/Bubble_Sort.js
+++ JavaScript/Bubble_Sort.js
@@ -1,2 +1,31 @@
+// Bubble Sort - 
+
+// Swaping Function -
+function swap(){
+    var Temp = arrToSort[First_Element];
+    arrToSort[First_Element] = arrToSort[Adjacent_Element];
+    arrToSort[Adjacent_Element] = arrToSort[First_Element];
+}
+
+// Bubble Sort Function -
+function bubbleSort(arrToSort){
+    // 2 loops -
+    // First loop takes First_Element
+    for (let First_Element = 0; First_Element < arrToSort.lenght; First_Element++){
+        // Second loop takes Adjacent_Element (i.e Next Element) -
+        for(let Adjacent_Element = 0; Adjacent_Element < arrToSort.lenght; Adjacent_Element++){
+            // Check If First_Element is < than Adjacent_Element 
+            // If Yes than swap those values -
+            swap(arrToSort[First_Element], arrToSort[Adjacent_Element], arrToSort)
+        }
+    } 
+    // return Sorted Array -
+    return arrToSort
+}
 
 
+// Take Array To Sort -
+var arrToSort = [2, 5, 0, 1, 7, 5];
+
+// Show User Sorted Array -
+console.log(bubbleSort(arrToSort));
--- Python/Bubble_Sort.py
+++ Python/Bubble_Sort.py
@@ -8,21 +8,32 @@ def swapValue(firstElement, adjacentElement, arrToSort):
 
 # function to bubble sort list -
 def bubbleSort(arrToSort):
+
+    # No Element Swaped Indicator - 
+    swaped = False
     
     # 2 loops with till lenght of list -
     # loop one carries firstElement
     for firstElement in range(0, len(arrToSort)):
     # loop two carries adjacentElement (Next Element in List)
-        for adjacentElement in range(0, len(arrToSort)):
-        # if firstElement value < adjacentElement 
-            if arrToSort[firstElement] < arrToSort[adjacentElement]:
+        # this loop runs and reduces its lenght every time -
+        for adjacentElement in range(0, len(arrToSort)-firstElement-1):
+        # if firstElement value > adjacentElement 
+            if arrToSort[adjacentElement] > arrToSort[adjacentElement+1]:
                 # swap those digits and save to list -
-                swapValue(firstElement, adjacentElement, arrToSort)
+                swapValue(adjacentElement, adjacentElement+1, arrToSort)
+                swaped = True
+
+        # if not element's swaped means array is already sorted
+        if swaped == False:
+            break
+
+
     # return sorted list
     return arrToSort
 
 # Take Data In -
-arrToSort = list(map(int, input().split(',')))[:]
+arrToSort = list(map(int, input().split()))[:]
 
 # print(len(arrToSort))
 print('Unsorted List = ', arrToSort, '\nSorted List =' ,bubbleSort(arrToSort))
